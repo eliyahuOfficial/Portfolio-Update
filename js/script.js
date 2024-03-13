ccc39d6e991208a3c8cb7a974fb63bb26efbc399
Eliyahu Levi <156104966+eliyahuOfficial@users.noreply.github.com>
2024-03-13 19:18:33 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    if ($('.typedn').length) {
+        var typedn = new Typed('.typedn', {
+
+            strings: ["Versatile full-stack web developer and music producer ... "],
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 2000
+        });
+    }
+});
+
+
+
+$(document).ready(function () {
+    if ($('.typedpara').length) {
+        var typedpara = new Typed('.typedpara', {
+
+            strings: ["Full Stack Web Developer ..."],
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 2000
+        });
+    }
+});
+
+
+$(document).ready(function () {
+    if ($('.typedprojects').length) {
+        var typedprojects = new Typed('.typedprojects', {
+
+            strings: ["Responsive landing pages - Html Css."],
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 2000
+        });
+    }
+});
+
+
+$(document).ready(function () {
+    if ($('.typedgames').length) {
+        var typedgames = new Typed('.typedgames', {
+
+            strings: ["Responsive game pages - Java Script."],
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 2000
+        });
+    }
+});
